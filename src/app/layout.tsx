93bfaaba78be277829aefad38d9b104b555c5418
Abdul Rehman <ar618211@gmail.com>
2023-08-20 00:18:30 +0500

    added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Streaming SSR Demo</title>
+        <meta name="description" content="Built with Next.js by Abdul Rehman" />
+        <meta property="og:url" content="https://streaming.abdulrehmandev.me/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Streaming SSR Demo" />
+        <meta property="og:description" content="Built with Next.js by Abdul Rehman" />
+        <meta property="og:image" content="/card.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="streaming.abdulrehmandev.me" />
+        <meta property="twitter:url" content="https://streaming.abdulrehmandev.me/" />
+        <meta name="twitter:title" content="Streaming SSR Demo" />
+        <meta name="twitter:description" content="Built with Next.js by Abdul Rehman" />
+        <meta name="twitter:image" content="/card.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
